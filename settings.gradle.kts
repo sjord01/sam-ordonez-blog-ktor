c5cb657791ca7cf8d369aafe05a081c08598d99e
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-04-04 15:43:47 -0700

    'initial commit'

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "sam-ordonez-blog-ktor"
---
+++ src/main/kotlin/com/samordonez/Application.kt
@@ -0,0 +1,13 @@
+package com.samordonez
+
+import com.samordonez.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureTemplating()
+    configureRouting()
+}
