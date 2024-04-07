4afda021e1aa6b90a0860da458d9208fe7db9249
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-04-07 15:17:50 -0700

    'added CRUD functionalities'

--- src/main/kotlin/com/samordonez/plugins/Routing.kt
+++ src/main/kotlin/com/samordonez/plugins/Routing.kt
@@ -28,15 +28,41 @@ fun Application.configureRouting() {
             }
             post {
                 // Save an article
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val newEntry = Article.newEntry(title, body)
+                articles.add(newEntry)
+                call.respondRedirect("/articles/${newEntry.id}")
             }
             get("{id}") {
                 // Show an article with a specific id
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("show.ftl", mapOf("article" to articles.find { it.id == id })))
             }
             get("{id}/edit") {
                 // Show a page with fields for editing an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("edit.ftl", mapOf("article" to articles.find { it.id == id })))
             }
             post("{id}") {
                 // Update or delete an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when (formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val index = articles.indexOf(articles.find { it.id == id })
+                        val title = formParameters.getOrFail("title")
+                        val body = formParameters.getOrFail("body")
+                        articles[index].title = title
+                        articles[index].body = body
+                        call.respondRedirect("/articles/$id")
+                    }
+                    "delete" -> {
+                        articles.removeIf { it.id == id }
+                        call.respondRedirect("/articles")
+                    }
+                }
             }
         }
     }
