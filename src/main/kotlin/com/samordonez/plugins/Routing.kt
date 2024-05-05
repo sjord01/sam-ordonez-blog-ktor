2b91179d0a4a89692d45875cbf5f722623579845
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-05-04 20:09:12 -0700

    'updated Routing.kt'

--- src/main/kotlin/com/samordonez/plugins/Routing.kt
+++ src/main/kotlin/com/samordonez/plugins/Routing.kt
@@ -1,11 +1,15 @@
 package com.samordonez.plugins
 
-import com.samordonez.dao.DAOFacade
-import com.samordonez.dao.DAOFacadeCacheImpl
-import com.samordonez.dao.DAOFacadeImpl
+import com.samordonez.dao.*
 import io.ktor.server.application.*
-import kotlinx.coroutines.runBlocking
-import java.io.File
+import io.ktor.server.freemarker.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.util.*
+import kotlinx.coroutines.*
+import java.io.*
 
 fun Application.configureRouting() {
     val dao: DAOFacade = DAOFacadeCacheImpl(
@@ -15,6 +19,54 @@ fun Application.configureRouting() {
         runBlocking {
             if(allArticles().isEmpty()) {
                 addNewArticle("The drive to develop!", "...it's what keeps me going.")
+            }
+        }
+    }
+    routing {
+        static("/static") {
+            resources("files")
+        }
+        get("/") {
+            call.respondRedirect("articles")
+        }
+
+        route("articles") {
+            get {
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to dao.allArticles())))
+            }
+            get("new") {
+                call.respond(FreeMarkerContent("new.ftl", model = null))
+            }
+            post {
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val article = dao.addNewArticle(title, body)
+                call.respondRedirect("/articles/${article?.id}")
+            }
+            get("{id}") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("show.ftl", mapOf("article" to dao.article(id))))
+            }
+            get("{id}/edit") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("edit.ftl", mapOf("article" to dao.article(id))))
+            }
+            post("{id}") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when (formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val title = formParameters.getOrFail("title")
+                        val body = formParameters.getOrFail("body")
+                        dao.editArticle(id, title, body)
+                        call.respondRedirect("/articles/$id")
+                    }
+                    "delete" -> {
+                        dao.deleteArticle(id)
+                        call.respondRedirect("/articles")
+                    }
+                }
             }
         }
     }
